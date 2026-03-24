396561749fd38e88805f4817c961fbdf5ec9f4b2
Hiroto Kanda <115915955+HirotoKanda@users.noreply.github.com>
2026-03-25 00:55:28 +0900

    Patched stderr error

--- .zshrc
+++ .zshrc
@@ -1,2 +1,2 @@
-[[ -o interactive && -t 1 && ! -t 2 ]] && exec 2>&1
 eval "$(sheldon source)"
+exec 2>&1
--- zsh/sync/startup.zsh
+++ zsh/sync/startup.zsh
@@ -1,2 +1,7 @@
 fastfetch
 ls
+
+restore_stderr() {
+  [[ -o interactive && -t 1 && ! -t 2 ]] && exec 2>&1
+}
+precmd_functions+=(restore_stderr)
